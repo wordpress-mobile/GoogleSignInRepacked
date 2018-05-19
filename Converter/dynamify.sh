25f86f3cf0e1ff8b9ca5a05ff6959bfef86abc65
Jorge Leandro Perez <jorge@lantean.co>
2018-05-19 18:01:19 -0300

    Updates Converter

--- Converter/dynamify.sh
+++ Converter/dynamify.sh
@@ -11,14 +11,14 @@
 ##
 ##
 DEPENDENCIES="-framework CoreText -framework SystemConfiguration -framework Security -framework Foundation -framework CoreGraphics \
-				-lSystem -framework UIKit -framework SafariServices -framework GTMOAuth2 -framework GTMSessionFetcher"
+				-lSystem -framework UIKit -framework SafariServices -framework GoogleSignInDependencies"
 SDK_DEVICE_PATH="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk"
 SDK_SIMULATOR_PATH="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk"
-DEVICE_DEPENDENCIES_PATH="../DependencyDevice/"
-SIMULATOR_DEPENDENCIES_PATH="../DependencySimulator/"
+DEPENDENCIES_PATH="../Original/"
+STATIC_LIB_PATH="Original/GoogleSignIn.framework/GoogleSignIn"
 
 
-lipo -info GoogleSignIn
+lipo -info $STATIC_LIB_PATH
 
 mkdir i386 
 mkdir x86_64
@@ -27,35 +27,34 @@ mkdir arm64
 
 pushd .
 cd arm64
-lipo ../GoogleSignIn.framework/GoogleSignIn -extract arm64 -output GoogleSignIn
+lipo ../$STATIC_LIB_PATH -extract arm64 -output GoogleSignIn
 lipo GoogleSignIn -thin arm64 -output GoogleSignInThin
 ar -x GoogleSignInThin
-libtool -dynamic *.o -o libGoogleSignIn_dynamic.dylib -ios_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_DEVICE_PATH -F$DEVICE_DEPENDENCIES_PATH -arch_only arm64
+libtool -dynamic *.o -o libGoogleSignIn_dynamic.dylib -ios_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_DEVICE_PATH -F$DEPENDENCIES_PATH -arch_only arm64
 popd
 
 pushd .
 cd armv7
-lipo ../GoogleSignIn.framework/GoogleSignIn -extract armv7 -output GoogleSignIn
+lipo ../$STATIC_LIB_PATH -extract armv7 -output GoogleSignIn
 lipo GoogleSignIn -thin armv7 -output GoogleSignInThin
 ar -x GoogleSignInThin
-libtool -dynamic *.o -o libGoogleSignIn_dynamic.dylib -ios_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_DEVICE_PATH -F$DEVICE_DEPENDENCIES_PATH -arch_only armv7
+libtool -dynamic *.o -o libGoogleSignIn_dynamic.dylib -ios_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_DEVICE_PATH -F$DEPENDENCIES_PATH -arch_only armv7
 popd
 
 pushd .
 cd x86_64
-ar -x GoogleSignInThin
-lipo ../GoogleSignIn.framework/GoogleSignIn -extract x86_64 -output GoogleSignIn
+lipo ../$STATIC_LIB_PATH -extract x86_64 -output GoogleSignIn
 lipo GoogleSignIn -thin x86_64 -output GoogleSignInThin
 ar -x GoogleSignInThin
-libtool -dynamic *.o -o libGoogleSignIn_dynamic.dylib -ios_simulator_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_SIMULATOR_PATH -F$SIMULATOR_DEPENDENCIES_PATH -arch_only x86_64
+libtool -dynamic *.o -o libGoogleSignIn_dynamic.dylib -ios_simulator_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_SIMULATOR_PATH -F$DEPENDENCIES_PATH -arch_only x86_64
 popd
 
 pushd .
 cd i386
-lipo ../GoogleSignIn.framework/GoogleSignIn -extract i386 -output GoogleSignIn
+lipo ../$STATIC_LIB_PATH -extract i386 -output GoogleSignIn
 lipo GoogleSignIn -thin i386 -output GoogleSignInThin
 ar -x GoogleSignInThin
-libtool -dynamic *.o -o libGoogleSignIn_dynamic.dylib -ios_simulator_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_SIMULATOR_PATH -F$SIMULATOR_DEPENDENCIES_PATH -arch_only i386
+libtool -dynamic *.o -o libGoogleSignIn_dynamic.dylib -ios_simulator_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_SIMULATOR_PATH -F$DEPENDENCIES_PATH -arch_only i386
 popd
 
 
