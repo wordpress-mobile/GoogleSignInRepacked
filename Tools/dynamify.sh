d18cb0c4465a91bd51523a81eb0c1c772dba3028
Jorge Leandro Perez <jorge@lantean.co>
2018-05-20 09:56:24 -0300

    Updating script

--- Tools/dynamify.sh
+++ Tools/dynamify.sh
@@ -1,3 +1,4 @@
+##
 ## 	Source: 	https://developers.google.com/identity/sign-in/ios/sdk/
 ## 	Specs: 		https://github.com/CocoaPods/Specs/blob/master/Specs/d/4/0/GoogleSignIn/4.1.2/GoogleSignIn.podspec.json
 ## 	Tutorial: 	https://pewpewthespells.com/blog/convert_static_to_dynamic.html
@@ -18,54 +19,82 @@ DEPENDENCIES_PATH="../Original/"
 STATIC_LIB_PATH="Original/GoogleSignIn.framework/GoogleSignIn"
 
 
+## Just for display: Current bundled architectures
+##
 lipo -info $STATIC_LIB_PATH
 
-mkdir i386 
-mkdir x86_64
-mkdir armv7
-mkdir arm64
 
-pushd .
-cd arm64
-lipo ../$STATIC_LIB_PATH -extract arm64 -output GoogleSignInStatic
-lipo GoogleSignInStatic -thin arm64 -output GoogleSignInThin
-ar -x GoogleSignInThin
-libtool -dynamic *.o -o GoogleSignIn -ios_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_DEVICE_PATH -F$DEPENDENCIES_PATH -arch_only arm64
-popd
+## Creates and access a New Folder
+##
+createAndAccessFolder() {
+	pushd .
+	mkdir ${1}
+	cd ${1}
+}
 
-pushd .
-cd armv7
-lipo ../$STATIC_LIB_PATH -extract armv7 -output GoogleSignInStatic
-lipo GoogleSignInStatic -thin armv7 -output GoogleSignInThin
-ar -x GoogleSignInThin
-libtool -dynamic *.o -o GoogleSignIn -ios_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_DEVICE_PATH -F$DEPENDENCIES_PATH -arch_only armv7
-popd
+## Restores the previously pushed folder
+##
+restorePreviousFolder() {
+	popd
+}
 
-pushd .
-cd x86_64
-lipo ../$STATIC_LIB_PATH -extract x86_64 -output GoogleSignInStatic
-lipo GoogleSignInStatic -thin x86_64 -output GoogleSignInThin
-ar -x GoogleSignInThin
-libtool -dynamic *.o -o GoogleSignIn -ios_simulator_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_SIMULATOR_PATH -F$DEPENDENCIES_PATH -arch_only x86_64
-popd
+## Extracts Object Files from the specified Static Library
+##
+## ${1}: Static Library Path
+## ${2}: Target Architecture
+##
+extractObjectFiles() {
+	lipo ${1} -extract ${2} -output Static
+	lipo Static -thin ${2} -output Thin
+	ar -x Thin
+}
 
-pushd .
-cd i386
-lipo ../$STATIC_LIB_PATH -extract i386 -output GoogleSignInStatic
-lipo GoogleSignInStatic -thin i386 -output GoogleSignInThin
-ar -x GoogleSignInThin
-libtool -dynamic *.o -o GoogleSignIn -ios_simulator_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_SIMULATOR_PATH -F$DEPENDENCIES_PATH -arch_only i386
-popd
+## Generates an ARM Dynamic Libary, starting off with a specified Static Library
+##
+## ${1}: Specific ARM Architecture
+##
+dynamifyARM() {
+	createAndAccessFolder ${1}
+
+	extractObjectFiles ../$STATIC_LIB_PATH ${1}
+	libtool -dynamic *.o -o GoogleSignIn -ios_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_DEVICE_PATH -F$DEPENDENCIES_PATH -arch_only ${1}
+
+	restorePreviousFolder
+}
+
+## Generates an Intel Dynamic Libary, starting off from the $STATIC_LIB_PATH
+##
+## ${1}: Specific Intel Architecture
+##
+dynamifyIntel() {
+	createAndAccessFolder ${1}
+
+	extractObjectFiles ../$STATIC_LIB_PATH ${1}
+	libtool -dynamic *.o -o GoogleSignIn -ios_simulator_version_min 8.0 $DEPENDENCIES -syslibroot $SDK_SIMULATOR_PATH -F$DEPENDENCIES_PATH -arch_only ${1}
+
+	restorePreviousFolder
+}
 
 
+## Generate Architecture Specific dylib's
+##
+dynamifyARM arm64
+dynamifyARM armv7
+dynamifyIntel i386
+dynamifyIntel x86_64
 
-lipo -create \
-	./i386/GoogleSignIn \
-	./x86_64/GoogleSignIn \
-	./arm64/GoogleSignIn \
-	./armv7/GoogleSignIn \
-	-output ./GoogleSignIn
 
+## Merge all of the architectures into a single dylib
+##
+lipo -create ./i386/GoogleSignIn ./x86_64/GoogleSignIn ./arm64/GoogleSignIn ./armv7/GoogleSignIn -output ./GoogleSignIn
+
+## Patch: Install name. Without this, you're DOOMED.
+##
+install_name_tool -id  "@rpath/GoogleSignIn.framework/GoogleSignIn" GoogleSignIn 
+otool -D GoogleSignIn 
+
+## Cleanup
+##
 rm -rf arm64
 rm -rf armv7
 rm -rf i386
